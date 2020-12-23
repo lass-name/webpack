763aa3ddbc36039cfa0731eab4e85e844cb234b8
辰跃 <wangwen@youbaolian.com>
2020-12-23 19:58:56 +0800

    修改模版测试输入表达式显示

--- template/src/components/Dialog.vue
+++ template/src/components/Dialog.vue
@@ -10,8 +10,10 @@
         <slot name="tips"></slot>
       </span>
       <slot name="actions">
+        {{#if_eq platform "web"}}
         <el-button @click="close">{{cancelButtonText}}</el-button>
         <el-button type="primary" @click="confirm">{{confirmButtonText}}</el-button>
+        {{/if_eq}}
       </slot>
     </template>
   </el-dialog>
